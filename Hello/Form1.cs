fd1167c98af9706f39b7cfd47cb7062651354d41
rc0033225 <rc0033225@CRO-24-03>
2018-09-11 09:30:05 +0100

    Changed hello to say goodbye.

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -19,7 +19,7 @@ namespace Hello
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello " + txtBxName.Text);
+            MessageBox.Show("Goodbye " + txtBxName.Text);
         }
 
         private void txtBxName_TextChanged(object sender, EventArgs e)
