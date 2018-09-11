8cc27123c7ff2e1f27e7b7cb917ba5be28ad97ef
rc0033225 <rc0033225@CRO-24-03>
2018-09-11 09:27:18 +0100

    Added a text box so the user can input something.

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -19,7 +19,12 @@ namespace Hello
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
+            MessageBox.Show("Hello " + txtBxName.Text);
+        }
+
+        private void txtBxName_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
